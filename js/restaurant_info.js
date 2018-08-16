89d5f0b16485c8ccf7fb7325576d56eac01dd599
EdnahM <edna.mugoh6@gmail.com>
2018-08-16 23:16:19 +0300

    Map Token key inserted

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZWRuYWgiLCJhIjoiY2prZHk4MHV3MG55aTNrcXNybWhvOWdiZiJ9.Y2fMW_p71AbsNI9fhgGvhA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
